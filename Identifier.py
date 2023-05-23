29804ab6e510f04993436e77d89c8a4684a04b73
Praveenakrishna <102336074+Praveenakrishna@users.noreply.github.com>
2023-05-23 13:39:23 +0000

    Timeitusage

---
+++ Identifier.py
@@ -0,0 +1,20 @@
+# name ="praveena"
+
+# print("name=", str(name))
+
+# print(True)
+# print(keyword.iskeyword("True"))
+# print(keyword.iskeyword("true"))
+# print(keyword.iskeyword("my_value"))
+
+true_value=123
+none_result="ntg"
+
+class_01="python identifiers"
+
+first_____C="Python"
+
+_names="someone"
+___rass="SAMe"
+__sal=34465476
+
